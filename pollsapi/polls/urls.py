d8519c7cebc83518af003858a42c1c3ceb52bb61
Maxim Kr <max.kriv0s@gmail.com>
2023-03-10 17:29:14 +0300

    day 2

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,2 +1,8 @@
+from django.urls import path
+from .views import polls_list, polls_details
+from .apiviews import PollList, PollDetails, ChoiceList, CreateVote
+
 urlpatterns = [
+    path('polls/<int:pk>/choices/', ChoiceList.as_view(), name='choices_list'),
+    path('polls/<int:pk>/choices/<int:choices_pk>/vote/', CreateVote.as_view(), name='create_vote')
 ]
--- pollsapi/polls/views.py
+++ pollsapi/polls/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from .models import Poll
 
-# Create your views here.
+
+def polls_list(request):
+    MAX_OBJECTS = 20
+    polls = Poll.objects.all()[:MAX_OBJECTS]
+    data = {"result": list(polls.values("pk","question", "create_by__username", "pub_date"))}
+    return JsonResponse(data)
+
+
+def polls_details(request, pk):
+    poll = get_object_or_404(Poll, pk=pk)
+    data = {"result": {
+        "question": poll.question,
+        "create_by__username": poll.create_by.username,
+        "pub_date": poll.pub_date
+    }}
+    return JsonResponse(data)
